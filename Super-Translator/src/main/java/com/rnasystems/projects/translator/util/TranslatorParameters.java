462f19c56fb06e0d6f150cea04a460072084adaf
Richard Osmar Leon Ingaruca <imporet.linet@gmail.com>
2013-01-20 19:50:19 -0500

    Commit del code fuente del proyecto

---
+++ Super-Translator/src/main/java/com/rnasystems/projects/translator/util/TranslatorParameters.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rnasystems.projects.translator.util;
+
+import com.rnasystems.api.linet.properties.Parameters;
+
+
+
+/**
+ *
+ * @author Richard Osmar Leon Ingaruca
+ */
+public class TranslatorParameters extends Parameters{
+    
+}
